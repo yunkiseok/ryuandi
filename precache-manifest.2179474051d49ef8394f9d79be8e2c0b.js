2e6f9e1bea44d99f4780f4a8d0060fc1c45573eb
yunkiseok <boooker@naver.com>
2020-07-15 16:05:15 +0900

    Updates

---
+++ precache-manifest.2179474051d49ef8394f9d79be8e2c0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcca10cd1d4ba3ad65b9c9577925545d",
+    "url": "/ryuandi/index.html"
+  },
+  {
+    "revision": "f995bc4ca146f630d3f4",
+    "url": "/ryuandi/static/css/main.0a5de1c5.chunk.css"
+  },
+  {
+    "revision": "b465fb3a41db387c5800",
+    "url": "/ryuandi/static/js/2.6760420d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ryuandi/static/js/2.6760420d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f995bc4ca146f630d3f4",
+    "url": "/ryuandi/static/js/main.0b8a3929.chunk.js"
+  },
+  {
+    "revision": "3e9ffb4b58fd397f3265",
+    "url": "/ryuandi/static/js/runtime-main.366e3555.js"
+  }
+]);
